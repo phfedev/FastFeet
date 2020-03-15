a418acd900d09d591b83fff2e775e060fc0a9756
Pedro Henrique <phfedev@gmail.com>
2020-03-15 19:47:36 -0300

    Added the restriction to add and update data if you are not logged in

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
 const RecipientController = require('./app/controllers/RecipientController');
+const SessionControlelr = require('./app/controllers/SessionController');
+const authMiddleware = require('./app/middlewares/auth');
 
 const routes = new Router();
 
+routes.post('/login', SessionControlelr.store);
+routes.use(authMiddleware);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients', RecipientController.update);
 module.exports = routes;
